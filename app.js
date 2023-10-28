caf4603b73364e9da9759a03b0e01c75fd3df851
vivek_bhuva <vivekpate26@gmail.com>
2023-10-28 10:27:01 +0530

    cors and morgon installations

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-var express = require("express");
+//
 
-var app = express();
+import cors from "cors";
+import express from "express";
+import morgon from "morgon";
 
+const app = express();
+
+/**middlewares */
+app.use(cors());
+app.use(morgon("dev"));
+
+/** Disable the X-Powered-By header*/
+app.disable("x-powered-by");
+
+/** Http get request */
 app.get("/", (req, res) => {
   res.send("working fine ");
 });
+
+/** start Server  */
 app.listen(5000, () => {
   "Server is started sucessfully";
 });
