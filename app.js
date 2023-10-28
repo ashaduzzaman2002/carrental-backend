285e1b423e06aac1bbff13faf7304554ee815342
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-10-28 10:30:48 +0530

    project setup

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var express = require("express");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
-var app = express();
+const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("working fine ");
 });
-app.listen(5000, () => {
-  "Server is started sucessfully";
-});
+
+// DB connect
+connectDB();
+
+app.listen(port, () =>
+  console.log(`Server is started sucessfully http://localhost:${port}`)
+);
